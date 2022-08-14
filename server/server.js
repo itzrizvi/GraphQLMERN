7ba6dedaed0d659a16360bd24c850f0e9b218471
itzrizvi <shahriar.me.work@gmail.com>
2022-08-15 03:19:02 +0600

    Environment Setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+
+app.listen(port, () => {
+    console.log(`SERVER IS RUNNING ON PORT: ${port}`);
+})
