048c41eab60bbebc27eceb66b2d6d870c96b7631
itzrizvi <shahriar.me.work@gmail.com>
2022-08-15 03:20:50 +0600

    Formation Setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+// ALL REQUIRES
 const express = require('express');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // PORT
 
 
+
+// CREATING APP
 const app = express();
 
 
+
+
+// SERVER LISTENING
 app.listen(port, () => {
     console.log(`SERVER IS RUNNING ON PORT: ${port}`);
 })
